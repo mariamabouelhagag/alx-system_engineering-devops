c325f27c66ea5532c8fe6d85159b6e222d442959
mariam salama <mariamsalama888@gmail.com>
2024-01-19 14:39:15 -0800

    modi

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This is a manifest for creating a file with specific properties
 
 file { '/tmp/school':
-    ensure => 'file',
-    mode   => '0744',
-    owner  => 'www-data',
-    group  => 'www-data',
-    content=> 'I love Puppet',
+ ensure  => 'file',
+ mode    => '0744',
+ owner   => 'www-data',
+ group   => 'www-data',
+ content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/pup
 #instual flask version 2.1.0
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+ ensure   => '2.1.0',
+ provider => 'pip3',
 }
