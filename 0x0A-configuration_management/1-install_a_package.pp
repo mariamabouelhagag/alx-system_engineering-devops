25503f882d2322bda22cc8dd38a5eb03b1e9785e
mariam salama <mariamsalama888@gmail.com>
2024-01-19 14:45:29 -0800

    modi

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-#instual flask version 2.1.0
-
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
